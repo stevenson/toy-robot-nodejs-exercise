9dc894c5b1318b68a6ff39966e7a7e25c938fd87
stevenson lee <stevenson.cunanan@gmail.com>
2022-05-25 15:33:09 +0800

    Update: move out things that can be encapsulated

--- index.js
+++ index.js
@@ -1,82 +1,26 @@
-const { read } = require('fs');
-var readline = require('readline');
+require('app-module-path/register');
 
-const { Robot } = require('./robot.js');
+const readline = require('readline');
+
+const { processCommand } = require('input-parser');
 
 let robot = null;
 
 const rl = readline.createInterface({
     input: process.stdin,
-    output: process.stdout
+    output: process.stdout,
 });
 
-function santizePlace(strX, strY, strF) {
-    const x = parseInt(strX.replace(/\D/g, '')) | null;
-    const y = parseInt(strY.replace(/\D/g, '')) | null;
-    const facing = strF.toLowerCase().match(/^((north|south)(east|west)?|east|west)/g);
-    if (0 <= x && x <= 5 && 0 <= y && y <= 5 && facing.length > 0) {
-        return { x, y, facing: facing[0] };
-    }
-    throw Error('invalid place parameters');
-}
-
-function performPlace(strX, strY, strF) {
-    const { x, y, facing } = santizePlace(strX, strY, strF);
-    robot = new Robot(x, y, facing);
-}
-
-
-
-function processCommand(command) {
-    const params = command.trim().split(/\s+/);
-    try {
-        switch (String(params[0].toLowerCase())) {
-            case 'place':
-                if (params.length = 4) {
-                    performPlace(params[1], params[2], params[3]);
-                } else {
-                    throw Error('place params require x y and direction');
-                }
-                break;
-            case 'move':
-                if (!robot) {
-                    throw Error('initialize location');
-                }
-                robot.move();
-                break;
-            case 'report':
-                if (!robot) {
-                    throw Error('initialize location');
-                }
-                robot.reportLocation();
-                break;
-            case 'left':
-                if (!robot) {
-                    throw Error('initialize location');
-                }
-                robot.left();
-                break;
-            case 'right':
-                if (!robot) {
-                    throw Error('initialize location');
-                }
-                robot.right();
-                break;
-            default:
-                console.log(`unknown command: ${params[0]}`);
-                break;
-        }
-    } catch (e) {
-        console.log(`invalid command: ${e.message}`);
-    }
-}
-
 function readUserInput() {
-    rl.question("Command: ", function (answer) {
-        if (answer == "exit") {
+    rl.question('Command: ', input => {
+        if (input === 'exit') {
             rl.close();
         } else {
-            processCommand(answer);
+            try {
+                robot = processCommand(input, robot);
+            } catch (e) {
+                console.log(`- ERROR: invalid command: ${e.message}`);
+            }
             readUserInput();
         }
     });
