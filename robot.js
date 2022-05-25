c276a2bc12897d411166efd4dfe9f4d576f7f089
stevenson lee <stevenson.cunanan@gmail.com>
2022-05-25 15:31:50 +0800

    Update: create robot model to encapsulate robot features

--- robot.js
+++ robot.js
@@ -1,120 +1,80 @@
-class Robot{
-    constructor(x,y,facing){
-        this.x=x;
-        this.y=y;
-        this.facing=facing;
-        this.reportLocation();
-    }
-    reportLocation(){
-        console.log(`- location at (${this.x},${this.y}) facing ${this.facing}.`)
-    }
-    left(){
-        switch(this.facing){
-            case 'north':
-                this.facing = 'west';
-                break;
-            case 'south':
-                this.facing = 'east';
-                break;
-            case 'east':
-                this.facing = 'north';
-                break;
-            case 'west':
-                this.facing = 'south';
-                break;
-            case 'northeast':
-                this.facing = 'northwest';
-                break;
-            case 'northwest':
-                this.facing = 'southwest';
-                break;
-            case 'southeast':
-                this.facing = 'northeast';
-                break;
-            case 'southwest':
-                this.facing = 'southeast';
-                break;
-        }
-        this.reportLocation();
-    }
-    right(){
-        switch(this.facing){
-            case 'north':
-                this.facing = 'east';
-                break;
-            case 'south':
-                this.facing = 'west';
-                break;
-            case 'east':
-                this.facing = 'south';
-                break;
-            case 'west':
-                this.facing = 'north';
-                break;
-            case 'northeast':
-                this.facing = 'southeast';
-                break;
-            case 'northwest':
-                this.facing = 'northeast';
-                break;
-            case 'southeast':
-                this.facing = 'southwest';
-                break;
-            case 'southwest':
-                this.facing = 'northwest';
-                break;
-        }
-        this.reportLocation();
-    }
-    move(){
-        let newX = this.x;
-        let newY = this.y;
-        switch(this.facing){
-            case 'north':
-                newY++;
-                break;
-            case 'south':
-                newY--;
-                break;
-            case 'east':
-                newX++;
-                break;
-            case 'west':
-                newX--;
-                break;
-            case 'northeast':
-                newY++;
-                newX++;
-                break;
-            case 'northwest':
-                newY++;
-                newX--;
-                break;
-            case 'southeast':
-                newY--;
-                newX++;
-                break;
-            case 'southwest':
-                newY--;
-                newX--;
-                break;
-        }
-        if(this.validateLocation(newX,newY)){
-            this.x=newX;
-            this.y=newY;
-            this.reportLocation();
-        }else{
-            throw Error(`invalid move: (${newX},${newY}) is out of bounds`);
-        }  
-    }
-    validateLocation(x,y){
-        if (0 <= x && x <= 5 && 0 <= y && y <= 5) {
+const dirs = [
+    'north',
+    'northeast',
+    'east',
+    'southeast',
+    'south',
+    'southwest',
+    'west',
+    'northwest',
+];
+
+const indices = {
+    north: 0,
+    northeast: 1,
+    east: 2,
+    southeast: 3,
+    south: 4,
+    southwest: 5,
+    west: 6,
+    northwest: 7,
+};
+
+class Robot {
+    validateLocation(x, y) {
+        if (x >= 0 && x <= 5 && y >= 0 && y <= 5) {
             return true;
         }
         return false;
     }
+
+    reportLocation() {
+        console.log(`- location at (${this.x},${this.y}) facing ${this.facing}.`)
+        return {
+            x: this.x,
+            y: this.y,
+            z: this.facing,
+        };
+    }
+
+    constructor(x, y, facing) {
+        this.x = x;
+        this.y = y;
+        this.facing = facing;
+        this.reportLocation();
+    }
+
+    left() {
+        const newIndex = (indices[this.facing] + 6) % 8;
+        this.facing = dirs[newIndex];
+        this.reportLocation();
+    }
+
+    right() {
+        const newIndex = (indices[this.facing] + 2) % 8;
+        this.facing = dirs[newIndex];
+        this.reportLocation();
+    }
+
+    move() {
+        let newX = this.x;
+        let newY = this.y;
+
+        if (this.facing.includes('north')) newY += 1;
+        if (this.facing.includes('south')) newY -= 1;
+        if (this.facing.includes('east')) newX += 1;
+        if (this.facing.includes('west')) newX -= 1;
+
+        if (this.validateLocation(newX, newY)) {
+            this.x = newX;
+            this.y = newY;
+            this.reportLocation();
+        } else {
+            throw Error(`invalid move: (${newX},${newY}) is out of bounds`);
+        }
+    }
 }
 
 module.exports = {
-    Robot
+    Robot,
 };
